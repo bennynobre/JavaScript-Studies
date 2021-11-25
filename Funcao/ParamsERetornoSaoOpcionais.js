b4ac1836b7316d7e51131782f5b4fcac29b6e275
Benny <bennynobre1@gmail.com>
2021-11-25 18:35:01 -0300

    adding javascript activities

---
+++ Funcao/ParamsERetornoSaoOpcionais.js
@@ -0,0 +1,65 @@
+/*function area(largura, altura) {
+    const area = largura * altura;
+    if (area > 20) {
+        console.log(`Valor acima do permitido: ${area}m2`)
+    } else {
+        return area
+    }
+}
+
+console.log(area(2, 2))
+console.log(area(2)) // NaN
+console.log(area()) // NaN
+console.log(area(2, 3, 17, 22, 44)) // NaN
+console.log(area(5, 5)) // Valor acima do permitido: 25m2
+
+
+/////////////////////////////////////////////////////////////////////////////
+// Parametros Variaveis
+
+function soma() {
+    let soma = 0
+    for (i in arguments) {
+        soma += arguments[i]
+    }
+    return soma
+}
+
+console.log(soma())
+console.log(soma(1))
+console.log(soma(1.1, 2.2, 3.3))
+
+console.log(soma(1.1, 2.2, 'Teste'))
+console.log(soma('a', 'b', 'c'))
+*/
+
+////////////////////////////////////////////////////////////////////////////////
+// Parametros Padrão
+
+// estratégia 1 para gerar valor padrão
+function soma1(a, b, c) {
+    a = a || 1
+    b = b || 1
+    c = c || 1
+    return a + b + c
+}
+
+console.log(soma1(), soma1(3), soma1(1, 2, 3), soma1(0, 0, 0))
+
+
+// estratégia 2, 3 e 4 para gerar valor padrão
+function soma2(a, b, c) {
+    a = a !== undefined ? a : 1
+    b = 1 in arguments ? b : 1
+    c = isNaN(c) ? 1 : c
+    return a + b + c
+}
+
+console.log(soma2(), soma2(3), soma2(1, 2, 3), soma2(0, 0, 0))
+
+// valor padrão do es2015
+function soma3(a = 1, b = 1, c = 1) { 
+    return a + b + c
+}
+
+console.log(soma3(), soma3(3), soma3(1, 2, 3), soma3(0, 0, 0))
